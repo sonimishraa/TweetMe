beb0958b55e846a1ea48442aaf0b520de8a2cc5c
Mishra Soni <>
2022-06-20 08:50:40 +0530

    update MainViewModel

--- app/src/main/java/com/soni/tweetme/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/soni/tweetme/viewmodel/MainViewModel.kt
@@ -1,15 +1,17 @@
 package com.soni.tweetme.viewmodel
 
 import android.app.Application
+import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.soni.tweetme.network.response.Tweet
 import com.soni.tweetme.network.response.User
-import com.soni.tweetme.repository.TweetRepository
 import com.soni.tweetme.utils.DATA_TWEETS
+import com.soni.tweetme.utils.DATA_TWEET_IMAGES
 import com.soni.tweetme.utils.DATA_TWEET_USER_IDS
 import com.soni.tweetme.utils.DATA_USERS
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,7 +20,6 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     val application: Application,
-    private val myRepository: TweetRepository,
 ) : ViewModel() {
     var currentUser: User? = null
     protected val firebaseDB = FirebaseFirestore.getInstance()
@@ -59,4 +60,86 @@ class MainViewModel @Inject constructor(
                 it.printStackTrace()
             }
     }
+
+    val tweetStatusMutableLiveData = MutableLiveData<String>()
+
+    fun postTweet(text: String, userName: String?, imageUrl: String?) {
+        val hashtags = getHashtags(text)
+        // POST: to update DB
+        val tweetId = firebaseDB.collection(DATA_TWEETS).document() // Create Tweets Collection
+        val tweet = Tweet(      // Tweet model
+            tweetId.id,
+            arrayListOf(userId),
+            userName,
+            text,
+            imageUrl,
+            System.currentTimeMillis(),
+            hashtags,
+            arrayListOf()
+        )
+
+        tweetId.set(tweet)
+            .addOnCompleteListener {
+                tweetStatusMutableLiveData.postValue("SUCCESS")
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+                tweetStatusMutableLiveData.postValue("FAIL")
+            }
+    }
+
+    fun getHashtags(source: String): ArrayList<String> {
+        val hashtags = arrayListOf<String>()
+        var text = source   // assign source into variable
+
+        while (text.contains("#")) {
+            var hashtag = ""
+            val hash = text.indexOf("#")
+            text = text.substring(hash + 1)
+
+            val firstSpace = text.indexOf(" ")
+            val firstHash = text.indexOf("#")
+
+            // Filter source of '#' and 'space'  -- Can use RegEx
+            if (firstSpace == -1 && firstHash == -1) {
+                hashtag = text.substring(0)
+            } else if (firstSpace != -1 && firstSpace < firstHash) {
+                hashtag = text.substring(0, firstSpace)
+                text = text.substring(firstSpace + 1)
+            } else {
+                hashtag = text.substring(0, firstHash)
+                text = text.substring(firstHash)
+            }
+
+            if (!hashtag.isNullOrEmpty()) {
+                hashtags.add(hashtag)
+            }
+        }
+        return hashtags
+    }
+
+    private val firebaseStorage = FirebaseStorage.getInstance().reference
+
+    fun storeImage(imageUri: Uri) {
+        val filePath = firebaseStorage.child(DATA_TWEET_IMAGES).child(userId)
+        // Save into Storage
+        filePath.putFile(imageUri)
+            .addOnSuccessListener {
+
+                // Get url from Storage >> DB
+                filePath.downloadUrl
+                    .addOnSuccessListener { uri ->
+                        tweetImageURIMutableLiveData.postValue(uri.toString())
+                    }
+                    .addOnFailureListener {
+                        tweetImageFailMutableLiveData.postValue(true)
+                    }
+            }
+            .addOnFailureListener {
+                tweetImageFailMutableLiveData.postValue(true)
+            }
+    }
+
+    val tweetImageFailMutableLiveData = MutableLiveData(false)
+    val tweetImageURIMutableLiveData = MutableLiveData<String>()
 }
